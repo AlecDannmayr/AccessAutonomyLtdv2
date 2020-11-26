8ec410eb16bd2e8ddc5f640c850a4e777d6ebd76
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-11-26 17:47:19 +0000

    Console Errors debugged

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ if(x.style.display==="none") {
     x.style.display="none";
 }
 }
+
+$(document).on('click', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) {
+      $(this).collapse('hide');
+    }
+  });
