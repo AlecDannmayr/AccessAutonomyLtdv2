278222a7904876b1bbc95e0f245a60e09f09e4a7
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-11-27 17:26:37 +0000

    Modified Script, Deleted Scroll.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 function myFunction() {
-  var x = document.getElementById("data-design");
-  if(x.style.display==="none") {
-      x.style.display="block";
-  } else {
-      x.style.display="none";
-  }
   var x = document.getElementById("iddesign");
   x.style.color = "#5496C1";
   x.style.borderBottom = "3px solid #5496C1";
