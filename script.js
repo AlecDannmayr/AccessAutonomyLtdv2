7a8fe834f990aa8d87c03615510d206db546efbe
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-11-27 14:33:34 +0000

    Revert to files from yesterday

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function myFunction() {
-var x = document.getElementById("data-design");
-if(x.style.display==="none") {
-    x.style.display="block";
-} else {
-    x.style.display="none";
-}
-var x = document.getElementById("iddesign");
-x.style.color = "#5496C1";
-x.style.borderBottom = "3px solid #5496C1";
-x.style.paddingbottom = "3px";
-}
-
-$(document).on('click', '.navbar-collapse.in', function (e) {
-    if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) {
-      $(this).collapse('hide');
-    }
-  });
+  var x = document.getElementById("data-design");
+  if(x.style.display==="none") {
+      x.style.display="block";
+  } else {
+      x.style.display="none";
+  }
+  var x = document.getElementById("iddesign");
+  x.style.color = "#5496C1";
+  x.style.borderBottom = "3px solid #5496C1";
+  x.style.paddingbottom = "3px";
+  }
+  
+  $(document).on('click', '.navbar-collapse.in', function (e) {
+      if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) {
+        $(this).collapse('hide');
+      }
+    });
