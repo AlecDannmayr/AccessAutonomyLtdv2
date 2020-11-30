e32453634f9a63d470cf6f9dcabffa0e62f3b09f
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-11-30 18:06:56 +0000

    Milstone: Before changing classes

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-function myFunction() {
-  var x = document.getElementById("design");
-  x.style.color = "#5496C1";
-  x.style.borderBottom = "3px solid #5496C1";
-  x.style.paddingbottom = "3px";
-  }
-  
-  $(document).on('click', '.navbar-collapse.in', function (e) {
+$(document).on('click', '.navbar-collapse.in', function (e) {
       if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) {
         $(this).collapse('hide');
       }
