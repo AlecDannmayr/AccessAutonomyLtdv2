584f6e00fd02447463b050507314781535ad3cd5
Alec Dannmayr <alec.dannmayr@gmail.com>
2020-12-01 11:39:02 +0000

    Font update

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-$(document).on('click', '.navbar-collapse.in', function (e) {
+function myFunction() {
+  var x = document.getElementById("design");
+  x.style.color = "#5496C1";
+  x.style.borderBottom = "3px solid #5496C1";
+  x.style.paddingbottom = "3px";
+  }
+  
+  $(document).on('click', '.navbar-collapse.in', function (e) {
       if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) {
         $(this).collapse('hide');
       }
